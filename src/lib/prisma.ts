dbd3afcfccdabaef43450d938c232e212defd691
whoshnri <henrybassey2007@gmail.com>
2026-06-05 12:08:02 +0100

    please

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { PrismaClient } from '../generated/client'
+import { PrismaClient } from '../generated/client.ts'
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
