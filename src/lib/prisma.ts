4b1e05b3a5cf2a0867424a68b3cd30fe4d7a3530
whoshnri <henrybassey2007@gmail.com>
2026-06-05 11:49:15 +0100

    feat: node kill me

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { PrismaClient } from '../generated/client.js'
+import { PrismaClient } from '../generated/client'
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
